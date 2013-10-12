31846c145f8da57b7e60052b628d65b44dd0bb02
Denis C <dtc8506@rit.edu>
2013-10-12 18:07:38 -0400

    fixed tabs to spaces

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var WsInterface   = require ('./network/interface/ws');
 //var ApiService    = require ('./network/service/api');
 var JsonService   = require ('./network/service/json');
 
-var api_v1  	  = require('./api/v1');
+var api_v1        = require('./api/v1');
 
 
 var websocket = new WsInterface (new JsonService (api_v1));
-	websocket.listen (30001, function ()
-	{
-		// server ready and listening
-	    console.log (websocket.server.address ());
-	});
+    websocket.listen (30001, function ()
+    {
+        // server ready and listening
+        console.log (websocket.server.address ());
+    });
